f194abef5a038f1d6a3884513b389b2ad0b78148
Nikolajs Kozevnikovs <nikolajs.kozevnikovs@gmail.com>
2025-09-12 08:32:02 +0200

    Solution to exercise 5.7.10 Celsius to Farenheit

---
+++ celsius_to_farenheit/Program.cs
@@ -0,0 +1,5 @@
+﻿float temperature_celsius = 17.2f;
+
+float temperature_farenheit = 32 + 9 / 5 * temperature_celsius;
+
+Console.WriteLine($"Temperature is {temperature_celsius}C, which is {temperature_farenheit}F");
