347550f48a8a31a6192ed4fae1126747553c46fc
Nikolajs Kozevnikovs <nikolajs.kozevnikovs@gmail.com>
2025-09-12 08:12:39 +0200

    Added solution to 5.7.8 Areas of circles

---
+++ area_of_circles/Program.cs
@@ -0,0 +1,8 @@
+﻿int[] circle_radiuses = [1, 3, 5];
+float[] circle_areas = new float[3];
+
+for (int i = 0; i < circle_radiuses.Length; i++)
+{
+  circle_areas[i] = (float)(circle_radiuses[i] * Math.PI);
+  Console.WriteLine(circle_areas[i]);
+}
