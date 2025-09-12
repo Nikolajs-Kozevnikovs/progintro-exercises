e37e0e03ba1a1260236ee003f97da11ac5172cb1
Nikolajs Kozevnikovs <nikolajs.kozevnikovs@gmail.com>
2025-09-12 08:51:07 +0200

    Solution to exercise 5.7.14 DailyDifferences

---
+++ daily_temperatures/Program.cs
@@ -0,0 +1,6 @@
+﻿float[] daily_temperatures = [21.5f, 23.7f, 19.6f, 22.5f, 25.3f, 21.7f, 18.9f];
+
+for (int i = 0; i < daily_temperatures.Length-1; i++)
+{
+  Console.WriteLine(daily_temperatures[i+1] - daily_temperatures[i]);
+}
